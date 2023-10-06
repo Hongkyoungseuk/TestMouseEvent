dcb8f82e43d32eaa4343e81c5c881d2aecd04051
{Hongkyoungseuk} <tug234@daum.net>
2023-10-06 13:12:55 +0900

    10.06 13:12

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        MouseEventFrame frame = new MouseEventFrame();
+
+        frame.setVisible(true);
+    }
+}
+ß
